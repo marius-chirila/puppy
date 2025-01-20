a67dc730144283ec726fffd982d9ebf30c3bdae6
Marius Chirila <marius.chirila.91@gmail.com>
2025-01-20 13:35:21 +0100

    When grown up

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/marius-chirila/puppy
 
 go 1.23.4
+
+require github.com/marius-chirila/dog v0.0.0-20250120123122-916335250fe3
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/marius-chirila/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrowUp(Barks())
 }
